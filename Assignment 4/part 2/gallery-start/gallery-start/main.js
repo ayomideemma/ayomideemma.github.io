84fe12cbd08637a0a1226aef5f17621653e8d1bd
ayomide emmanuel <ayomideemmnauel338@gmail.com>
2025-04-02 10:57:11 -0400

    Update main.js
    /* Declaring the array of image filenames *//* Declaring the alternative text for each image file */

--- Assignment 4/part 2/gallery-start/gallery-start/main.js
+++ Assignment 4/part 2/gallery-start/gallery-start/main.js
@@ -8,12 +8,21 @@
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
+
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageFiles = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
+const altText = {
+    'pic1.jpg': 'A scenic mountain view',
+    'pic2.jpg': 'A beautiful sunset over the ocean',
+    'pic3.jpg': 'A lush green forest',
+    'pic4.jpg': 'A city skyline at night',
+    'pic5.jpg': 'A serene desert landscape'
+};
 
 /* Looping through images */
 
