fe348671ad24af8785393a433818f7e88a26bf2e
ayomide emmanuel <ayomideemmnauel338@gmail.com>
2025-04-02 11:20:19 -0400

    part 2 final
    final commit for part 2

--- Assignment 4/part 2/gallery-start/gallery-start/main.js
+++ Assignment 4/part 2/gallery-start/gallery-start/main.js
@@ -2,7 +2,7 @@
 // File: index.html
 // Date: 18 march 2025
 // Student ID: 100994931
-// Description: A button generates a random silly story, replacing "Bob" with a custom name if provided and converting US units to UK equivalents if the UK option is selected.
+// Description: Giving the ability of darken/whiten button effect on a image by making it darker and lighter at the click of the darken/whiten button.
 
 
 const displayedImage = document.querySelector('.displayed-img');
