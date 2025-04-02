baf261880cd0c1f4bb2edbf8f3e297fb9387a5af
ayomide emmanuel <ayomideemmnauel338@gmail.com>
2025-04-02 10:42:23 -0400

    update assignment 4
    comment header added to all the file

--- Assignment 4/part 2/gallery-start/gallery-start/main.js
+++ Assignment 4/part 2/gallery-start/gallery-start/main.js
@@ -1,3 +1,10 @@
+// Name: Aderinboye Ayomide Emmnauel
+// File: index.html
+// Date: 18 march 2025
+// Student ID: 100994931
+// Description: A button generates a random silly story, replacing "Bob" with a custom name if provided and converting US units to UK equivalents if the UK option is selected.
+
+
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
