cade33774e4b475e6c1b78571822bd7c92259a45
ayomide emmanuel <ayomideemmnauel338@gmail.com>
2025-04-02 11:02:06 -0400

    Update main.js
    // Wiring up the Darken/Lighten button

--- Assignment 4/part 2/gallery-start/gallery-start/main.js
+++ Assignment 4/part 2/gallery-start/gallery-start/main.js
@@ -39,4 +39,15 @@ imageFiles.forEach(image => {
     });
 });
 
-/* Wiring up the Darken/Lighten button */
+// Wiring up the Darken/Lighten button
+btn.addEventListener('click', () => {
+    if (btn.getAttribute('class') === 'dark') {
+        btn.setAttribute('class', 'light');
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    } else {
+        btn.setAttribute('class', 'dark');
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor = 'rgb(0 0 0 / 0%)';
+    }
+});
